b5042b6de77af3696b62264e083bb5ac13ef33c8
Christoffer Lervåg <chris.lervag@gmail.com>
2021-01-29 14:26:40 +0100

    Clean up ROI and bone overlap

--- cobra_dl_quality_control.py
+++ cobra_dl_quality_control.py
@@ -26,11 +26,45 @@ failures = []
 # A function for extracting an item from a script collection by its name parameter.
 # Returns the item if there is a match, if not returns None.
 def get_item(collection, item_name):
+  # Verify input:
+  assert isinstance(item_name, str), "item_name is not a string: %r" % item_name
   match = None
   for item in collection:
     if item.Name == item_name:
       match = item
   return match
+
+# A function for removing the overlap between two volumes.
+def remove_overlap(master_rois, yielding_roi):
+  # Verify input:
+  assert isinstance(master_rois, list), "master_rois is not a list: %r" % master_rois
+  assert isinstance(yielding_roi, str), "yielding_roi is not a string: %r" % yielding_roi
+  temp = "TempROI"
+  # Make sure our temp ROI doesnt exist:
+  if get_item(pm.RegionsOfInterest, temp):
+    pm.RegionsOfInterest[temp].DeleteRoi()
+  # Create a copy of the yielding_roi:
+  pm.CreateRoi(Name=temp, Color="#FFFF0000", Type="Organ")
+  pm.RegionsOfInterest[temp].CreateMarginGeometry(Examination=examination, SourceRoiName=yielding_roi, MarginSettings = { 'Type': "Contract", 'Superior' : 0.0, 'Inferior': 0.0, 'Anterior': 0.0, 'Posterior': 0.0, 'Right': 0.0, 'Left': 0.0})
+  # Use ROI Algebra to create a ROI where the yielding ROI is pulled out of the master ROI:
+  pm.RegionsOfInterest[yielding_roi].CreateAlgebraGeometry(
+    Examination = examination,
+    ExpressionA =  { 'Operation': "Union", 'SourceRoiNames': [temp], 'MarginSettings': { 'Type': "Expand", 'Superior': 0.0, 'Inferior': 0.0, 'Anterior': 0.0, 'Posterior': 0.0, 'Right': 0.0, 'Left': 0.0 } },
+    ExpressionB = { 'Operation': "Union", 'SourceRoiNames': master_rois, 'MarginSettings': { 'Type': "Expand", 'Superior': 0.0, 'Inferior': 0.0, 'Anterior': 0.0, 'Posterior': 0.0, 'Right': 0.0, 'Left': 0.0 } },
+    ResultOperation ='Subtraction',
+    ResultMarginSettings =  { 'Type': "Contract", 'Superior' : 0.0, 'Inferior': 0.0, 'Anterior': 0.0, 'Posterior': 0.0, 'Right': 0.0, 'Left': 0.0 }
+  )
+  pm.RegionsOfInterest[yielding_roi].SetAlgebraExpression(
+    ExpressionA =  { 'Operation': "Union", 'SourceRoiNames': [temp], 'MarginSettings': { 'Type': "Expand", 'Superior': 0.0, 'Inferior': 0.0, 'Anterior': 0.0, 'Posterior': 0.0, 'Right': 0.0, 'Left': 0.0 } },
+    ExpressionB = { 'Operation': "Union", 'SourceRoiNames': master_rois, 'MarginSettings': { 'Type': "Expand", 'Superior': 0.0, 'Inferior': 0.0, 'Anterior': 0.0, 'Posterior': 0.0, 'Right': 0.0, 'Left': 0.0 } },
+    ResultOperation ='Subtraction',
+    ResultMarginSettings =  { 'Type': "Contract", 'Superior' : 0.0, 'Inferior': 0.0, 'Anterior': 0.0, 'Posterior': 0.0, 'Right': 0.0, 'Left': 0.0 }
+  )
+  pm.RegionsOfInterest[yielding_roi].UpdateDerivedGeometry(Examination = examination)
+  # At the end, underive it, as it isnt supposed to be a derived ROI:
+  pm.RegionsOfInterest[yielding_roi].DeleteExpression()
+  # Finally, delete the temp ROI:
+  pm.RegionsOfInterest[temp].DeleteRoi()
 
 # Some cleanup for Ålesund:
 # Rename/switch spinal canal ROIs for Ålesund:
@@ -50,7 +84,45 @@ if d2:
 r3 = get_item(pm.RegionsOfInterest, "ScaleneMusc_Ant")
 if r3:
   r3.Name = 'ScaleneMusc_Ant_L'
-  
+# Remove some common overlaps between neighbouring ROIs:
+# From Lungs and Esophagus, extract Heart:
+remove_overlap(["Lung_R", "Lung_L", "Esophagus"], "Heart")
+# From Lungs and Trachea, extract Esophagus:
+remove_overlap(["Lung_R", "Lung_L", "Trachea"], "Esophagus")
+# From Lung_L, extract A_Subclavian_L+A_Axillary_L:
+remove_overlap(["Lung_L"], "A_Subclavian_L+A_Axillary_L")
+# From Lungs, extract V_Brachioceph_L:
+remove_overlap(["Lung_R", "Lung_L"], "V_Brachioceph_L")
+# From Sternum and Lung_L, extract LN_IMN_L:
+remove_overlap(["Sternum", "Lung_L"], "LN_IMN_L")
+# From Breast_L, extract LN_Ax_L1_L:
+remove_overlap(["Breast_L"], "LN_Ax_L1_L")
+# From LN_Ax_L1_L, extract LN_Ax_L2_L:
+remove_overlap(["LN_Ax_L1_L"], "LN_Ax_L2_L")
+# From LN_Ax_L3_L, extract LN_Ax_L2_L:
+remove_overlap(["LN_Ax_L3_L"], "LN_Ax_L2_L")
+# From LN_Ax_L3_L, extract LN_Ax_L4_L:
+remove_overlap(["LN_Ax_L3_L"], "LN_Ax_L4_L")
+# From LN_Ax_L1_L and LN_Ax_L2_L and LN_Ax_L3_L, extract LN_Ax_Pectoral_L:
+remove_overlap(["LN_Ax_L1_L", "LN_Ax_L2_L", "LN_Ax_L3_L"], "LN_Ax_Pectoral_L")
+# From A_Subclavian_L+A_Axillary_L, extract ScaleneMusc_Ant_L:
+remove_overlap(["A_Subclavian_L+A_Axillary_L"], "ScaleneMusc_Ant_L")
+# From Trachea and A_Carotid_L and V_Jugular_L, extract ThyroidGland:
+remove_overlap(["Trachea", "A_Carotid_L", "V_Jugular_L"], "ThyroidGland")
+# From A_Carotid_L, extract V_Subclavian_L+V_Axillary_L:
+remove_overlap(["A_Carotid_L"], "V_Subclavian_L+V_Axillary_L")
+# Remove bony density areas from the SpinalCanal (and SpinalCanalFull):
+# Create a bony ROI by density:
+bone = get_item(pm.RegionsOfInterest, "Bone")
+if bone:
+  bone.DeleteRoi()
+bone = pm.CreateRoi(Name = 'Bone', Color = 'Yellow', Type = 'Undefined')
+bone.GrayLevelThreshold(Examination=examination, LowThreshold=250, HighThreshold=3071, PetUnit=r"", CbctUnit=None, BoundingBox=None)
+# From the bony density ROI, extract SpinalCanal and SpinalCanalFull:
+remove_overlap(["Bone"], "SpinalCanal")
+remove_overlap(["Bone"], "SpinalCanalFull")
+bone.DeleteRoi()
+
 # ROIs which are used to derive ROIs (they are accepted to be present, but not required to be):
 temp_rois = [
   'Breast_L_Draft',
