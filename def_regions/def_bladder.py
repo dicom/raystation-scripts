89ba80c915724dec873055b9f801f645b47b920a
Christoffer Lervåg <chris.lervag@gmail.com>
2021-09-01 14:20:28 +0200

    Minor rearrangement

--- def_regions/def_bladder.py
+++ def_regions/def_bladder.py
@@ -12,9 +12,11 @@ class DefBladder(object):
 
   # Adds target and OAR ROIs to the given site and creates them in RayStation.
   def __init__(self, pm, examination, ss, choices, site):
-    site.add_oars(DEF.bladder_oars)
+    # Targets:
     ctv =  ROI.ROIAlgebra(ROIS.ctv.name, ROIS.ctv.type, COLORS.ctv_low, sourcesA=[ROIS.gtv], sourcesB=[ROIS.bladder])
     ptv = ROI.ROIAlgebra(ROIS.ptv.name, ROIS.ptv.type, ROIS.ptv.color, sourcesA = [ctv], sourcesB = [ROIS.external], operator = 'Intersection', marginsA = MARGINS.bladder_expansion, marginsB = MARGINS.uniform_5mm_contraction)
     site.add_targets([ROIS.gtv, ctv, ptv])
+    # OARs:
+    site.add_oars(DEF.bladder_oars)
     # Create all targets and OARs in RayStation:
     site.create_rois()
