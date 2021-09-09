81e7507afcdbbc6fcc06ddbfef627fb6a3ab52b6
Christoffer Lervåg <chris.lervag@gmail.com>
2021-09-09 15:23:33 +0200

    Some cleanup of def_palliative

--- def_regions/def_palliative.py
+++ def_regions/def_palliative.py
@@ -16,28 +16,32 @@ class DefPalliative(object):
     stereotactic = choices[1]
     # Choice 2: Region
     region = choices[2]
-    # Stereotactic:
     if stereotactic == 'yes':
+      # Stereotactic:
       if region in ['col thorax', 'col pelvis']:
-        if region == 'col thorax':
-          site.add_oars(DEF.palliative_stereotactic_thorax_oars)
-        elif region == 'col pelvis':
-          site.add_oars(DEF.palliative_stereotactic_spine_pelvis_oars)
+        # Targets:
         ctv = ROI.ROIExpanded(ROIS.ctv.name, ROIS.ctv.type, COLORS.ctv_med, ROIS.gtv, margins = MARGINS.uniform_3mm_expansion)
         ptv = ROI.ROIAlgebra(ROIS.ptv.name, ROIS.ptv.type, ROIS.ptv.color, sourcesA=[ctv], sourcesB=[ROIS.vb])
         ptv_gtv = ROI.ROIAlgebra(ROIS.ptv_gtv.name, ROIS.ptv_gtv.type, COLORS.ptv_med, sourcesA = [ptv], sourcesB = [ROIS.gtv], operator='Subtraction')
         ptv_spinal = ROI.ROIAlgebra(ROIS.ptv_spinal.name, ROIS.ptv_spinal.type, COLORS.ptv_med, sourcesA = [ptv], sourcesB = [ROIS.spinal_cord_prv], operator='Subtraction', marginsA = MARGINS.zero, marginsB = MARGINS.uniform_1mm_expansion)
+        site.add_targets([ROIS.gtv, ROIS.vb, ptv_gtv, ptv_spinal, ctv, ptv])
+        # OARs:
+        if region == 'col thorax':
+          site.add_oars(DEF.palliative_stereotactic_thorax_oars)
+        elif region == 'col pelvis':
+          site.add_oars(DEF.palliative_stereotactic_spine_pelvis_oars)
         wall_ptv = ROI.ROIWall(ROIS.wall_ptv.name, ROIS.wall_ptv.type, COLORS.wall, ptv, 1, 0)
         site.add_oars([ROIS.spinal_cord_prv, wall_ptv])
-        site.add_targets([ROIS.gtv, ROIS.vb, ptv_gtv, ptv_spinal, ctv, ptv])
       else:
-        site.add_oars(DEF.palliative_stereotactic_pelvis_oars)
+        # Non-columna SBRT (assumed pelvis):
+        # Targets:
         ptv = ROI.ROIExpanded(ROIS.ptv.name, ROIS.ptv.type, ROIS.ptv.color, ROIS.gtv, margins = MARGINS.uniform_3mm_expansion)
-        wall_ptv = ROI.ROIWall(ROIS.wall_ptv.name, ROIS.wall_ptv.type, COLORS.wall, ptv, 1, 0)
-        site.add_oars([wall_ptv])
         site.add_targets([ROIS.gtv, ptv])
-    # Not stereotactic:
+        # OARs:
+        wall_ptv = ROI.ROIWall(ROIS.wall_ptv.name, ROIS.wall_ptv.type, COLORS.wall, ptv, 1, 0)
+        site.add_oars(DEF.palliative_stereotactic_pelvis_oars + [wall_ptv])   
     else:
+      # Non-stereotactic:
       # Region:
       if region == 'head':
         site.add_oars(DEF.palliative_head_oars)
@@ -56,10 +60,10 @@ class DefPalliative(object):
       elif region == 'pelvis':
         site.add_oars(DEF.palliative_pelvis_oars)
       # Choice 3: Number of targets:
-      nr_targets = choices[3]
+      nr_targets = int(choices[3])
       # Choice 4: GTV included?
       with_gtv = choices[4]
-      # Set PTV margins to use:
+      # Determine PTV margins:
       if with_gtv == 'with':
         # Soft tissue target:
         if region in ['head', 'neck']:
@@ -83,7 +87,7 @@ class DefPalliative(object):
           # Other torso: 5 mm
           ptv_margin = MARGINS.uniform_5mm_expansion
       # Set up target volumes:
-      if nr_targets == '1':
+      if nr_targets == 1:
         # A single target:
         if with_gtv == 'with':
           site.add_targets([ROIS.gtv, ROIS.ctv_ext])
@@ -91,35 +95,29 @@ class DefPalliative(object):
           site.add_targets([ROIS.ctv_underived])
         site.add_targets([ROI.ROIAlgebra(ROIS.ptv.name, ROIS.ptv.type, ROIS.ptv.color, sourcesA = [ROIS.ctv], sourcesB = [ROIS.external], operator = 'Intersection', marginsA = ptv_margin, marginsB = MARGINS.uniform_5mm_contraction)])
       else:
-        # 2 or 3 targets:
+        # Multiple targets (2 or 3):
+        gtvs = []
+        ctvs = []
+        ptvs = []
         if with_gtv=='with':
           # With GTV:
-          ctv1 = ROI.ROIAlgebra(ROIS.ctv1.name, ROIS.ctv1.type, ROIS.ctv.color, sourcesA = [ROIS.gtv1], sourcesB = [ROIS.external], operator = 'Intersection', marginsA = ptv_margin, marginsB = MARGINS.uniform_5mm_contraction)
-          ctv2 = ROI.ROIAlgebra(ROIS.ctv2.name, ROIS.ctv2.type, ROIS.ctv.color, sourcesA = [ROIS.gtv2], sourcesB = [ROIS.external], operator = 'Intersection', marginsA = ptv_margin, marginsB = MARGINS.uniform_5mm_contraction)
-          ptv1 = ROI.ROIAlgebra(ROIS.ptv1.name, ROIS.ptv1.type, ROIS.ptv.color, sourcesA = [ROIS.ctv1], sourcesB = [ROIS.external], operator = 'Intersection', marginsA = ptv_margin, marginsB = MARGINS.uniform_5mm_contraction)
-          ptv2 = ROI.ROIAlgebra(ROIS.ptv2.name, ROIS.ptv2.type, ROIS.ptv.color, sourcesA = [ROIS.ctv2], sourcesB = [ROIS.external], operator = 'Intersection', marginsA = ptv_margin, marginsB = MARGINS.uniform_5mm_contraction)
-          gtv =  ROI.ROIAlgebra(ROIS.gtv.name, ROIS.gtv.type, ROIS.gtv.color, sourcesA=[ROIS.gtv1], sourcesB=[ROIS.gtv2])
-          ctv =  ROI.ROIAlgebra(ROIS.ctv.name, ROIS.ctv.type, ROIS.ctv.color, sourcesA=[ctv1], sourcesB=[ctv2])
-          ptv =  ROI.ROIAlgebra(ROIS.ptv.name, ROIS.ptv.type, ROIS.ptv.color, sourcesA=[ptv1], sourcesB=[ptv2])
-          site.add_targets([ROIS.gtv1, ROIS.gtv2, gtv, ctv1, ctv2, ctv, ptv1, ptv2, ptv])
-          if nr_targets == '3':
-            ctv3 = ROI.ROIAlgebra(ROIS.ctv3.name, ROIS.ctv3.type, ROIS.ctv.color, sourcesA = [ROIS.gtv3], sourcesB = [ROIS.external], operator = 'Intersection', marginsA = ptv_margin, marginsB = MARGINS.uniform_5mm_contraction)
-            ptv3 = ROI.ROIAlgebra(ROIS.ptv3.name, ROIS.ptv3.type, ROIS.ptv.color, sourcesA = [ROIS.ctv3], sourcesB = [ROIS.external], operator = 'Intersection', marginsA = ptv_margin, marginsB = MARGINS.uniform_5mm_contraction)
-            gtv.sourcesB.extend([ROIS.gtv3])
-            ctv.sourcesB.extend([ctv3])
-            ptv.sourcesB.extend([ptv3])
-            site.add_targets([ROIS.gtv3, ctv3, ptv3])
+          for i in range(0, nr_targets):
+            gtvs.append(ROI.ROI('GTV'+str(i+1), 'Gtv', COLORS.gtv))
+            ctvs.append(ROI.ROIAlgebra(ROIS.ctv.name+str(i+1), ROIS.ctv1.type, ROIS.ctv.color, sourcesA = [gtvs[-1]], sourcesB = [ROIS.external], operator = 'Intersection', marginsA = ptv_margin, marginsB = MARGINS.uniform_5mm_contraction))
+            ptvs.append(ROI.ROIAlgebra(ROIS.ptv.name+str(i+1), ROIS.ptv1.type, ROIS.ptv.color, sourcesA = [ctvs[-1]], sourcesB = [ROIS.external], operator = 'Intersection', marginsA = ptv_margin, marginsB = MARGINS.uniform_5mm_contraction))
+          # GTV union target volume:
+          gtvs.append(ROI.ROIAlgebra(ROIS.gtv.name, ROIS.gtv.type, ROIS.gtv.color, sourcesA=[gtvs[0]], sourcesB=gtvs[1:]))
+          site.add_targets(gtvs)
         else:
           # Without GTV:
-          ptv1 = ROI.ROIAlgebra(ROIS.ptv1.name, ROIS.ptv1.type, ROIS.ptv.color, sourcesA = [ROIS.ctv1], sourcesB = [ROIS.external], operator = 'Intersection', marginsA = ptv_margin, marginsB = MARGINS.uniform_5mm_contraction)
-          ptv2 = ROI.ROIAlgebra(ROIS.ptv2.name, ROIS.ptv2.type, ROIS.ptv.color, sourcesA = [ROIS.ctv2], sourcesB = [ROIS.external], operator = 'Intersection', marginsA = ptv_margin, marginsB = MARGINS.uniform_5mm_contraction)
-          ctv =  ROI.ROIAlgebra(ROIS.ctv.name, ROIS.ctv.type, ROIS.ctv.color, sourcesA=[ROIS.ctv1], sourcesB=[ROIS.ctv2])
-          ptv =  ROI.ROIAlgebra(ROIS.ptv.name, ROIS.ptv.type, ROIS.ptv.color, sourcesA=[ptv1], sourcesB=[ptv2])
-          site.add_targets([ROIS.ctv1, ROIS.ctv2, ctv, ptv1, ptv2, ptv])
-          if nr_targets == '3':
-            ptv3 = ROI.ROIAlgebra(ROIS.ptv3.name, ROIS.ptv3.type, ROIS.ptv.color, sourcesA = [ROIS.ctv3], sourcesB = [ROIS.external], operator = 'Intersection', marginsA = ptv_margin, marginsB = MARGINS.uniform_5mm_contraction)
-            ctv.sourcesB.extend([ROIS.ctv3])
-            ptv.sourcesB.extend([ptv3])
-            site.add_targets([ROIS.ctv3, ptv3])
+          for i in range(0, nr_targets):
+            ctvs.append(ROI.ROI('CTV'+str(i+1), 'Ctv', COLORS.ctv))
+            ptvs.append(ROI.ROIAlgebra(ROIS.ptv.name+str(i+1), ROIS.ptv.type, ROIS.ptv.color, sourcesA = [ctvs[-1]], sourcesB = [ROIS.external], operator = 'Intersection', marginsA = ptv_margin, marginsB = MARGINS.uniform_5mm_contraction))
+        # Union target volumes:
+        ctv = ROI.ROIAlgebra(ROIS.ctv.name, ROIS.ctv.type, ROIS.ctv.color, sourcesA=[ctvs[0]], sourcesB=ctvs[1:])
+        ptv = ROI.ROIAlgebra(ROIS.ptv.name, ROIS.ptv.type, ROIS.ptv.color, sourcesA=[ptvs[0]], sourcesB=ptvs[1:])
+        ctvs.append(ctv)
+        ptvs.append(ptv)
+        site.add_targets(ctvs + ptvs)
     # Create all targets and OARs in RayStation:
     site.create_rois()
