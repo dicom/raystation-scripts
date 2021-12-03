139f914dfb99ced26af8246f280482b7f0a20c86
Christoffer Lervåg <chris.lervag@gmail.com>
2021-12-03 14:21:43 +0100

    Add option for resolution in QA preparation
    A GUI dialogue will now appear where the user can choose between the default VMAT QA dose grid (2 mm) or enhanced resolution (1mm) for SBRT.

--- create_and_export_qa_plan.py
+++ create_and_export_qa_plan.py
@@ -2,18 +2,31 @@
 # Creates a QA plan with standard settings and exports it to specified folder.
 #
 # Authors:
-# Marit Funderud
+# Christoffer Lervåg & Marit Funderud
 # Helse Møre og Romsdal HF
 #
-# Made for RayStation version: 9A
+# Made for RayStation version: 10B
 # Python 3.6
 
 # Import system libraries:
 from connect import *
 import clr, sys, os
-
 import math
+from tkinter import *
+from tkinter import messagebox
 
+# Add necessary folders to the system path:
+sys.path.append("C:\\temp\\raystation-scripts\\def_regions")
+sys.path.append("C:\\temp\\raystation-scripts\\functions")
+sys.path.append("C:\\temp\\raystation-scripts\\gui_classes")
+sys.path.append("C:\\temp\\raystation-scripts\\rt_classes")
+sys.path.append("C:\\temp\\raystation-scripts\\settings")
+
+# Import local files:
+import radio_button_frame as FRAME
+import gui_functions as GUIF
+import property as P
+import radio_button as RB
 
 # Load patient and case data:
 try:
@@ -29,37 +42,68 @@ try:
 except SystemError:
     raise IOError("No beam set loaded.")
 
+# Determine (unique) name of QA plan:
 name = 'QA'
-org_name = 'QA'
+prefix = 'QA'
 name_conflict = False
 if len(list(plan.VerificationPlans)) > 0:
   for p in plan.VerificationPlans:
-    if p.BeamSet.DicomPlanLabel == org_name:
+    if p.BeamSet.DicomPlanLabel == name:
       name_conflict = True
   if name_conflict:
     i = 0
     while True:
       i += 1
-      name = org_name + " " + str(i) + ""
-      name_conflict = False
+      name = prefix + " " + str(i)
+      available = True
       for p in plan.VerificationPlans:
         if p.BeamSet.DicomPlanLabel == name:
-          name_conflict = True
-      if name_conflict == False:
+          available = False
+      if available:
         break
 
+# Radiobutton choices for dose grid:
+c2mm = P.Property('Konvensjonell VMAT (2 mm)', '0.2', default = True)
+c1mm = P.Property('Stereotaksi (1 mm)','0.1')
+choices = [c2mm, c1mm]
+
+# Create radio button object
+options = RB.RadioButton('VMAT QA','Velg dosematrise:', choices)
+
+# Setup and run GUI:
+my_window = Tk()
+frame = FRAME.RadioButtonFrame(my_window, options)
+frame.grid(row = 0,column = 0)
+my_window.mainloop()
+
+# Extract information from the users's selections in the GUI:
+if frame.ok:
+    (selection,value) = frame.get_results()
+elif not frame.ok:
+    print ("Script execution cancelled by user...")
+    sys.exit(0)
+else:
+    messagebox.showinfo("Error.","Unexpected error.")
+    sys.exit(0)
+
+# Convert the selected value to a float:
+resolution = float(value)
+
+# Create QA plan:
 beam_set.CreateQAPlan(
 	PhantomName = 'ArcCheck (Heterogen bordtopp)',
 	PhantomId = 'AC_plug_test',
 	QAPlanName = name, 
 	IsoCenter = { 'x': -0.0, 'y': 0.0, 'z': 0 }, 
-	DoseGrid = { 'x': 0.2, 'y': 0.2, 'z': 0.2 }, 
+	DoseGrid = { 'x': resolution, 'y': resolution, 'z': resolution }, 
 	ComputeDoseWhenPlanIsCreated = True
 )
 
-# Save
+# Save:
 patient.Save()
 last_plan = len(list(plan.VerificationPlans))-1
+
+# DICOM export:
 plan.VerificationPlans[last_plan].ScriptableQADicomExport(
 	ExportFolderPath = "I:\\HSM - Kreftavdelingen - gammelt fellesområde\\Stråleterapi_avd\\Stråleterapi\\Mosaiq\\dokument-import\\VMAT_QA",
 	QaPlanIdentity = 'Patient',
@@ -70,5 +114,3 @@ plan.VerificationPlans[last_plan].ScriptableQADicomExport(
 	ExportBeamSetBeamDose = True,
 	IgnorePreConditionWarnings = False
 )
-
-
