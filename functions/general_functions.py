e56f71c7751fc6fae8bd911cfb17714b00199b72
Christoffer Lervåg <chris.lervag@gmail.com>
2022-08-31 15:01:44 +0200

    Clean up

--- functions/general_functions.py
+++ functions/general_functions.py
@@ -5,6 +5,5 @@
 
 # Dynamically rounds a float to position of the last non-zero decimal.
 # Returns the rounded value as a string.
-# FIXME: Bør antakeligvis flyttes ut i en felles fil.
 def dynamic_round(value):
   return str(int(float(value)) if int(float(value)) == float(value) else float(value))
