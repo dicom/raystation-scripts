8d5c1cd35b03dbb0505e4ceceb0e4eabe82f21e3
Christoffer Lervåg <chris.lervag@gmail.com>
2023-02-28 14:22:26 +0100

    Modified colors
    Changed colors for ctv to darker shades of pink.
    
    Changed colors to brain and nerve structures in the brain to shades of blue.
    
    Rearranged the OARs in the colors file according to anatomical categories.

--- settings/colors.py
+++ settings/colors.py
@@ -4,91 +4,106 @@
 
 # Target colors:
 gtv = 'Red'
-ctv = 'Pink'
+ctv = 'Salmon'
 ptv = '0, 128, 255'
 ptv_breast = 'LightSkyBlue'
-ctv_alt = 'LightPink'
-ctv_low = 'Pink'
-ctv_med = 'Violet'
-ctv_high = 'Salmon'
+ctv_alt = 'HotPink'
+ctv_low = 'HotPink'
+ctv_med = 'Salmon'
+ctv_high = 'Magenta'
 ptv_low = '0, 64, 128'
 ptv_med = '0, 128, 255'
 ptv_high = '0, 0, 128'
 
 # OAR colors:
-brain = 'Yellow'
-brainstem = 'Khaki'
-brainstem_core = 'DarkKhaki'
-brainstem_surface = 'DarkOrange'
+# Skin:
+skin = 'White'
+# Bone:
+caput ='Teal'
+rib = 'Navy'
+ribs = 'MidnightBlue'
+femoral = 'ForestGreen'
+# CNS:
+brain = 'DarkBlue'
+brainstem = 'Indigo'
+brainstem_core = 'MidnightBlue'
+brainstem_surface = 'RoyalBlue'
+hippocampus = 'Sienna'
+spinal_cord = 'Blue'
+spinal_canal = 'Blue'
+cauda = 'Blue'
+brachial = 'Orange'
+# Eye/Optic nerves:
 eye = 'DeepSkyBlue'
 lens = 'DarkTurquoise'
-optic_nrv = 'SaddleBrown'
-chiasma = 'Brown'
+cornea = 'Yellow'
+retina = 'OrangeRed'
+optic_nrv = 'SteelBlue'
+chiasma = 'MidnightBlue'
 lacrimal = 'DarkBlue'
+# Auditory:
 cochlea = 'Violet'
-hippocampus = 'Sienna'
+# Oral/nasal:
 nasal_cavity = 'OliveDrab'
 oral_cavity = 'Olive'
+# Glands:
+pituitary =  'Orange'
 parotid = 'Orange'
 submand = 'DarkOrange'
 thyroid = 'Teal'
-spinal_cord = 'Blue'
-spinal_canal = 'Blue'
-heart = 'Maroon'
+# Muscles:
+muscle = 'Darkcyan'
+# Respiratory:
+trachea = 'Teal'
+main_bronchus = 'Green'
 lungs = 'Green'
 lung = 'Lime'
-kidney = 'LightSalmon'
-kidneys = 'LightSalmon'
-bowel_space = 'SandyBrown'
-bladder = 'Yellow'
-rectum = 'SaddleBrown'
-anal_canal = 'Brown'
-esophagus = 'Orange'
-pituitary =  'Orange'
+# Breast:
+breast_draft = 'Salmon'
+contralat = '128, 128, 255'
+contralat_draft = '255, 192, 203'
+chestwall = 'Olive'
+# Cardiac:
+heart = 'Maroon'
+lad = 'Red'
+# Vascular:
 artery1 = 'Maroon'
 artery2 = 'Red'
 vein1 = 'Navy'
 vein2 = 'Blue'
-muscle = 'Darkcyan'
-contralat = '128, 128, 255'
-contralat_draft = '255, 192, 203'
-lad = 'Red'
-caput ='Teal'
-breast_draft = 'Salmon'
+# Gastrointenstinal:
+esophagus = 'Orange'
+stomach = 'SandyBrown'
+bowel_space = 'SandyBrown'
+rectum = 'SaddleBrown'
+anal_canal = 'Brown'
+bowel_contrast = 'Gray'
+dorso_rectum = 'SaddleBrown'
+small_bowel = 'SandyBrown'
+colon = 'SandyBrown'
+# Abdominal:
+pancreas = 'Coral'
+spleen = 'Crimson'
+liver = 'Khaki'
+# Urinary:
+kidney = 'LightSalmon'
+kidneys = 'LightSalmon'
+bladder = 'Yellow'
+urethra = 'Orange'
+# Lymph nodes:
 level4 = 'Sienna'
 level3 = 'Navy'
 level2 = 'Maroon'
 level1 = 'Khaki'
 level = 'DarkSlateGray'
 imn = 'Maroon'
-clips = 'Magenta'
 pelvic_nodes = 'Magenta'
+# Genital:
 prostate = 'DeepPink'
 prostate_bed = 'DeepPink'
 vesicles = 'Violet'
 penile_bulb = 'PaleVioletRed'
-femoral = 'ForestGreen'
-bowel_contrast = 'Gray'
-dorso_rectum = 'SaddleBrown'
-seed = 'Blue'
-wall = '128, 128, 128'
-chestwall = 'Olive'
-trachea = 'Teal'
-stomach = 'SandyBrown'
-pancreas = 'Coral'
-spleen = 'Crimson'
-liver = 'Khaki'
-rib = 'Navy'
-ribs = 'MidnightBlue'
-main_bronchus = 'Green'
-skin = 'White'
-cauda = 'Blue'
-small_bowel = 'SandyBrown'
-colon = 'SandyBrown'
-brachial = 'Orange'
-cornea = 'Yellow'
-retina = 'OrangeRed'
-urethra = 'Orange'
+
 
 # PRVs:
 prv = '128, 128, 128'
@@ -97,11 +112,16 @@ other_ptv = '0, 128, 128'
 # External:
 external = '255, 173, 91'
 
-# Support
+# Support:
 couch = 'Blue'
 
-# Other:
+# Others:
 mask_ptv = 'Black'
+wall = '128, 128, 128'
+
+# Markers/Prosthesis:
+clips = 'Magenta'
+seed = 'Blue'
 prosthesis = '255, 173, 91'
 
 # POIs:
