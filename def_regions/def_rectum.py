77242879e3a528c378ff0c4c3183e5a62ee4b5e1
Christoffer Lervåg <chris.lervag@gmail.com>
2023-04-13 10:02:17 +0200

    Slightly tweak Rectum 5x5 target definition
    No longer by dedfault use CTVp in the ROI algebra.

--- def_regions/def_rectum.py
+++ def_regions/def_rectum.py
@@ -60,11 +60,11 @@ class DefRectum(object):
       # Hypofractionated treatment (5 Gy x 5):
       # Targets:
       gtv = ROI.ROIAlgebra(ROIS.gtv.name, ROIS.gtv.type, ROIS.gtv.color, sourcesA=[ROIS.gtv_p], sourcesB=[ROIS.gtv_n1])
-      ctv_p_default = ROI.ROIExpanded('CTVp_default', ROIS.ctv_p.type, COLORS.ctv_high, source = gtv, margins = MARGINS.uniform_10mm_expansion)
-      ctv_p = ROI.ROIAlgebra(ROIS.ctv_p.name, ROIS.ctv_p.type, COLORS.ctv_high, sourcesA=[gtv], sourcesB=[ROIS.ctv_e], operator = 'Intersection', marginsA = MARGINS.uniform_10mm_expansion)
-      ctv = ROI.ROIAlgebra(ROIS.ctv.name, ROIS.ctv.type, COLORS.ctv_low, sourcesA=[ROIS.ctv_e], sourcesB=[ctv_p])
-      ptv = ROI.ROIAlgebra(ROIS.ptv.name, ROIS.ptv.type, COLORS.ptv_med, sourcesA=[ctv_p], sourcesB=[ROIS.ctv_e], marginsA = MARGINS.rectum_ptv_50_expansion, marginsB = MARGINS.rectum_ctv_primary_risk_expansion)
-      site.add_targets([ROIS.gtv_p, ROIS.gtv_n1, gtv, ctv_p_default, ctv_p, ROIS.ctv_e, ctv, ptv])
+      # We will not use the CTVp in ROI algebra from now on, but we'll keep it present for visual aid:
+      z_ctv_p_default = ROI.ROIExpanded('zCTVp_default', 'Undefined', COLORS.ctv_high, source = gtv, margins = MARGINS.uniform_10mm_expansion)
+      ctv = ROI.ROIAlgebra(ROIS.ctv.name, ROIS.ctv.type, COLORS.ctv_low, sourcesA=[ROIS.ctv_e], sourcesB=[ROIS.external], operator = 'Intersection', marginsB = MARGINS.uniform_5mm_contraction)
+      ptv = ROI.ROIExpanded(ROIS.ptv.name, ROIS.ptv.type, COLORS.ptv_med, source=ROIS.ctv_e, margins = MARGINS.rectum_ctv_primary_risk_expansion)
+      site.add_targets([ROIS.gtv_p, ROIS.gtv_n1, gtv, z_ctv_p_default, ROIS.ctv_e, ctv, ptv])
       # OARs:
       bladder_ptv = ROI.ROIAlgebra(ROIS.z_bladder.name, ROIS.z_bladder.type, COLORS.bladder, sourcesA = [ROIS.bladder], sourcesB = [ptv], operator='Subtraction', marginsB = MARGINS.uniform_3mm_expansion)
       bowel_ptv = ROI.ROIAlgebra(ROIS.z_spc_bowel.name, ROIS.z_spc_bowel.type, COLORS.bowel_space, sourcesA = [ROIS.bowel_space], sourcesB = [ptv], operator='Subtraction', marginsB = MARGINS.uniform_3mm_expansion)
