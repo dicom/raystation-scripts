1f25f44dfd0bc8f7f2a06aa835ea13d8a024aac4
Christoffer Lervåg <chris.lervag@gmail.com>
2024-11-20 15:18:15 +0100

    Add objectives for rectum 50 Gy
    Objectives for SIB 47 & 50 Gy treatment.

--- settings/objectives/rectum.py
+++ settings/objectives/rectum.py
@@ -49,8 +49,13 @@ class Rectum:
   # Create other objectives (e.q. External).
   def create_other_objectives(self, ss, plan, prescription, i):
     others = []
-    others.append(OF.fall_off(ss, plan, ROIS.external.name, prescription.total_dose*100, prescription.total_dose*100/2, 1.8, 15, beam_set_index=i))
-    others.append(OF.max_dose(ss, plan, ROIS.external.name, prescription.total_dose*100*1.05, 20, beam_set_index=i))
+    if prescription.total_dose == 50:
+      # SIB treatment (47 & 50 Gy):
+      others.append(OF.fall_off(ss, plan, ROIS.z_ptv_50_wall.name, 49*100, 45*100, 0.65, 5, adapt=True, beam_set_index=i))
+      others.append(OF.fall_off(ss, plan, ROIS.z_ptv_47_50_wall.name, 50*100, 32*100, 1.0, 5, adapt=True, beam_set_index=i))
+    else:
+      others.append(OF.fall_off(ss, plan, ROIS.external.name, prescription.total_dose*100, prescription.total_dose*100/2, 1.8, 15, beam_set_index=i))
+      others.append(OF.max_dose(ss, plan, ROIS.external.name, prescription.total_dose*100*1.05, 20, beam_set_index=i))
     # Return objectives (filtered for possible None elements):
     return [i for i in others if i is not None]
   
@@ -58,9 +63,18 @@ class Rectum:
   # Create target objectives.
   def create_target_objectives(self, ss, plan, prescription, i):
     targets = []
-    targets.append(OF.uniform_dose(ss, plan, ROIS.ctv.name, prescription.total_dose*100, 35, beam_set_index=i))
-    targets.append(OF.min_dose(ss, plan, ROIS.ptv.name, prescription.total_dose*100*0.95, 150, beam_set_index=i))
-    targets.append(OF.max_dose(ss, plan, ROIS.ptv.name, prescription.total_dose*100*1.05, 50, beam_set_index=i))
+    if prescription.total_dose == 50:
+      # SIB treatment (47 & 50 Gy):
+      targets.append(OF.uniform_dose(ss, plan, ROIS.ctv_50.name, prescription.total_dose*100, 35, beam_set_index=i))
+      targets.append(OF.min_dose(ss, plan, ROIS.ptv_50.name, prescription.total_dose*100*0.95, 150, beam_set_index=i))
+      targets.append(OF.max_dose(ss, plan, ROIS.ptv_50.name, prescription.total_dose*100*1.05, 50, beam_set_index=i))
+      targets.append(OF.uniform_dose(ss, plan, ROIS.ctv_47.name, 47*100, 35, beam_set_index=i))
+      targets.append(OF.min_dose(ss, plan, ROIS.ptv_47.name, 47*100*0.95, 150, beam_set_index=i))
+      targets.append(OF.max_dvh(ss, plan, ROIS.ptv_47.name, 47.5*100, 3, 125, beam_set_index=i))
+    else:
+      targets.append(OF.uniform_dose(ss, plan, ROIS.ctv.name, prescription.total_dose*100, 35, beam_set_index=i))
+      targets.append(OF.min_dose(ss, plan, ROIS.ptv.name, prescription.total_dose*100*0.95, 150, beam_set_index=i))
+      targets.append(OF.max_dose(ss, plan, ROIS.ptv.name, prescription.total_dose*100*1.05, 50, beam_set_index=i))
     # Return objectives (filtered for possible None elements):
     return [i for i in targets if i is not None]
   
