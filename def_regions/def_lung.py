fde449af51bb0a14a70886fe696d8a4c3bf25128
Christoffer Lervåg <chris.lervag@gmail.com>
2025-02-26 10:35:01 +0100

    Add pulmonary vessels to lung cases

--- def_regions/def_lung.py
+++ def_regions/def_lung.py
@@ -94,7 +94,7 @@ class DefLung(object):
       }
     })
     # Non-DL OARs:
-    site.add_oars([ROIS.lungs])
+    site.add_oars([ROIS.lungs, ROIS.a_pulmonary, ROIS.v_pulmonary])
   
   
   # Adds curative 4DCT/DIBH ROIs to the site object.
