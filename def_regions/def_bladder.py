76fed36442527287cace98258f90b138089192b0
Christoffer Lervåg <chris.lervag@gmail.com>
2025-11-20 15:35:04 +0100

    Implement ROIDLS for bladder region

--- def_regions/def_bladder.py
+++ def_regions/def_bladder.py
@@ -36,30 +36,11 @@ class DefBladder(object):
 
   # Adds rois that are common across all cases.
   def add_common_rois(self, pm, examination, site):
-    # DL OARs:
-    examination.RunDeepLearningSegmentationWithCustomRoiNames(ModelAndRoiNames={
-      'RSL DLS CT': {
-        "Bladder": "Bladder"
-      },
-      'Alesund Male Pelvic CT': {
-        "AnalCanal": "AnalCanal",
-        "BowelBag_Draft": "BowelBag",
-        "CaudaEquina": "CaudaEquina",
-        "Coccyx": "Coccyx",
-        "FemurHeadNeck_L": "FemurHeadNeck_L",
-        "FemurHeadNeck_R": "FemurHeadNeck_R",
-        "L5": "L5",
-        "PelvicGirdle_L": "PelvicGirdle_L",
-        "PelvicGirdle_R": "PelvicGirdle_R",
-        "Rectum": "Rectum",
-        "Sacrum": "Sacrum"
-      }
-    })
     # Create "Bone" ROI Algebra:
-    bone_rois = [ROIS.pelvic_girdle_l, ROIS.pelvic_girdle_r, ROIS.femur_head_neck_l, ROIS.femur_head_neck_r]
+    pelvic_bone_rois = [ROIS.pelvic_girdle_l, ROIS.pelvic_girdle_r, ROIS.femur_head_neck_l, ROIS.femur_head_neck_r]
     vertebrae_rois = [ROIS.l5, ROIS.sacrum, ROIS.coccyx]
-    bone = ROI.ROIAlgebra("Bone", 'Organ', COLORS.bone_color1, sourcesA = bone_rois, sourcesB = vertebrae_rois)
-    site.add_oars([bone, ROIS.bowel_bag])
+    bone = ROI.ROIAlgebra("Bone", 'Organ', COLORS.bone_color1, sourcesA = pelvic_bone_rois, sourcesB = vertebrae_rois)
+    site.add_oars([ROIS.anal_canal, ROIS.bladder, bone, ROIS.bowel_bag_draft, ROIS.bowel_bag, ROIS.cauda_equina, ROIS.coccyx, ROIS.l5, ROIS.femur_head_neck_l, ROIS.femur_head_neck_r, ROIS.pelvic_girdle_l, ROIS.pelvic_girdle_r, ROIS.rectum, ROIS.sacrum])
   
   
   # Adds rois that are based on gender.
@@ -67,12 +48,7 @@ class DefBladder(object):
     if patient.Gender == 'Female':
       site.add_oars([ROIS.uterus])
     elif patient.Gender == 'Male':
-      # DL OARs:
-      examination.RunDeepLearningSegmentationWithCustomRoiNames(ModelAndRoiNames={
-        'Alesund Male Pelvic CT': {
-          "PenileBulb": "PenileBulb"
-        }
-      })
+      site.add_oars([ROIS.penile_bulb])
   
   
   # Adds curative ROIs to the site object.
